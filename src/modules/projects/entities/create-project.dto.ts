1603e5f97d995a826d5fc2a38957e0bd7b505398
shedrackisrael54-maker <shedrackisrael54@gmail.com>
2026-08-01 21:58:49 +0100

    Update create-project.dto.ts

--- src/modules/projects/entities/create-project.dto.ts
+++ src/modules/projects/entities/create-project.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class CreateProjectDto {
   @IsString()
@@ -10,4 +10,8 @@ export class CreateProjectDto {
   @IsNotEmpty()
   @MaxLength(4000)
   prompt: string;
+
+  @IsOptional()
+  @IsString()
+  imageBase64?: string;
 }
